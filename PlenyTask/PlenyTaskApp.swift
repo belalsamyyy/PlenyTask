581564d99aba2ece5669f061aec98836b9509da4
Belal Samy <belalsamy10@gmail.com>
2023-09-14 20:41:17 +0300

    Initial Commit

---
+++ PlenyTask/PlenyTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlenyTaskApp.swift
+//  PlenyTask
+//
+//  Created by Belal Samy on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PlenyTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
