27c2115eb430852ace1e5f76bed4bfa1c3ba0f7a
Belal Samy <belalsamy10@gmail.com>
2023-09-14 22:15:49 +0300

    Add AppDelegate & SceneDelegate files * to use it as entry point for our app * setup third-party libraries like Firebase (for example) * Manage navigation & coordinator with UIKit-like code

---
+++ PlenyTask/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  PlenyTask
+//
+//  Created by Belal Samy on 9/14/23.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+@main
+class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject  {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("App Did Launch!")
+        return true
+    }
+
+    
+    // MARK: - UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+         let sceneConfig: UISceneConfiguration = UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+         sceneConfig.delegateClass = SceneDelegate.self
+         return sceneConfig
+    }
+}
---
+++ PlenyTask/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  PlenyTask
+//
+//  Created by Belal Samy on 9/14/23.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+class SceneDelegate: NSObject, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        print("SceneDelegate is connected!")
+    }
+    
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+    
+}
